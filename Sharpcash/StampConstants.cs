e0376420857da8621c4fbb406ba6e598513296ee
Philip Mørch <mrphil2105@gmail.com>
2022-09-19 13:50:25 +0200

    Define Hashcash stamp constants and regex

---
+++ Sharpcash/StampConstants.cs
@@ -0,0 +1,24 @@
+using System.Globalization;
+
+namespace Sharpcash;
+
+internal class StampConstants
+{
+    public const int VersionLength = 1;
+    public const int BitsLength = 2;
+    public const int DateLength = 6;
+
+    public const int ColonLength = 1;
+    public const int ColonCount = 6;
+
+    // Version + Bits + Date + 6 Colons (e.g. 1:20:081216::::)
+    public const int BasicLength = VersionLength + BitsLength + DateLength + ColonLength * ColonCount;
+    public const int MaxCounterLength = 12;
+
+    public const string DateFormat = "yyMMdd";
+
+    public const string StampStringPattern =
+        @"^[\d]:[\d]{2}:[\d]{6}:[^:]*::[^:]*:(?=.{0,12}$)(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$";
+
+    public static readonly IFormatProvider FormatProvider = CultureInfo.GetCultureInfo("en-US");
+}
