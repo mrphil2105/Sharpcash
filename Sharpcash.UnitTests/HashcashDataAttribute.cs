d0ecb59f1581b51eea7fbdb01090812d82c7c81b
Philip Mørch <mrphil2105@gmail.com>
2022-10-28 10:40:57 +0200

    Provide attribute for generating Hashcash data

---
+++ Sharpcash.UnitTests/HashcashDataAttribute.cs
@@ -0,0 +1,20 @@
+using AutoFixture;
+using AutoFixture.Xunit2;
+
+namespace Sharpcash.UnitTests;
+
+public class HashcashDataAttribute : AutoDataAttribute
+{
+    public HashcashDataAttribute() : base(CreateFixture)
+    {
+    }
+
+    private static IFixture CreateFixture()
+    {
+        var fixture = new Fixture();
+        fixture.Register((DateTime date, string resource, string random, long counter) =>
+            new HashcashStamp(10, date, resource, random, counter));
+
+        return fixture;
+    }
+}
