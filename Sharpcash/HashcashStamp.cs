d867867edcfff9c3bf68121dd08c6d665b3038c2
Philip Mørch <mrphil2105@gmail.com>
2022-10-28 10:54:19 +0200

    Implement Parse and TryParse methods

--- Sharpcash/HashcashStamp.cs
+++ Sharpcash/HashcashStamp.cs
@@ -1,5 +1,6 @@
 using System.Buffers;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using Sharpcash.Helpers;
 
@@ -44,6 +45,53 @@ public class HashcashStamp
     public string Random { get; }
 
     public long Counter { get; }
+
+    public static HashcashStamp Parse(ReadOnlySpan<char> stampChars)
+    {
+        if (!TryParse(stampChars, out var stamp))
+        {
+            throw new ArgumentException("The specified hashcash stamp is invalid.", nameof(stampChars));
+        }
+
+        return stamp;
+    }
+
+    public static bool TryParse(ReadOnlySpan<char> stampChars, [NotNullWhen(true)] out HashcashStamp? stamp)
+    {
+        if (!StampHelper.VerifyStampString(stampChars))
+        {
+            stamp = null;
+
+            return false;
+        }
+
+        var index = StampConstants.VersionLength + StampConstants.ColonLength;
+        var bitsChars = stampChars.Slice(index, StampConstants.BitsLength);
+        var bits = int.Parse(bitsChars);
+
+        index += StampConstants.BitsLength + StampConstants.ColonLength;
+        var dateChars = stampChars.Slice(index, StampConstants.DateLength);
+        var date = DateTime.ParseExact(dateChars, StampConstants.DateFormat, StampConstants.FormatProvider);
+
+        index += StampConstants.DateLength + StampConstants.ColonLength;
+        var resourceLength = stampChars[index..]
+            .IndexOf(':');
+        var resourceChars = stampChars.Slice(index, resourceLength);
+
+        // Multiply colon length by two to skip Extension.
+        index += resourceLength + StampConstants.ColonLength * 2;
+        var randomLength = stampChars[index..]
+            .IndexOf(':');
+        var randomChars = stampChars.Slice(index, randomLength);
+
+        index += randomLength + StampConstants.ColonLength;
+        var counterChars = stampChars[index..];
+        var counter = BuffersHelper.GetInt64(counterChars);
+
+        stamp = new HashcashStamp(bits, date, new string(resourceChars), new string(randomChars), counter);
+
+        return true;
+    }
 
     public bool TryFormat(Span<char> destination, out int charsWritten)
     {
