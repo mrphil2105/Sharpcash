cb14bfa2ceb1ec603402a79f8e8e41b5b3d8a5da
Philip Mørch <mrphil2105@gmail.com>
2023-10-24 15:18:50 +0200

    test: add stamp mint tests

--- Sharpcash.UnitTests/HashcashStampTests.cs
+++ Sharpcash.UnitTests/HashcashStampTests.cs
@@ -1,7 +1,51 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Sharpcash.UnitTests;
 
 public class HashcashStampTests
 {
+    private static readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA256;
+
+    [Theory]
+    [HashcashData]
+    public async Task MintAsync_CreatesZeroBits_SingleThreaded(HashcashStamp stamp)
+    {
+        var bytesToCheck = stamp.Bits / 8;
+        var remainderBits = stamp.Bits % 8;
+        var remainderMask = (byte)(0xFF << (8 - remainderBits));
+
+        var mintedStamp = await stamp.MintAsync(_hashAlgorithm);
+
+        using var hasher = CryptoHelper.CreateHashAlgorithm(_hashAlgorithm);
+        var stampString = mintedStamp.ToString();
+        var stampBytes = Encoding.UTF8.GetBytes(stampString);
+        var hash = hasher!.ComputeHash(stampBytes);
+
+        hash.Take(bytesToCheck).Should().OnlyContain(b => b == 0);
+        (hash[bytesToCheck] & remainderMask).Should().Be(0);
+    }
+
+    [Theory]
+    [HashcashData]
+    public async Task MintAsync_MintsStamp_SingleThreaded(HashcashStamp stamp)
+    {
+        var mintedStamp = await stamp.MintAsync(_hashAlgorithm);
+
+        var isValid = mintedStamp.Verify(_hashAlgorithm);
+        isValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [HashcashData]
+    public async Task MintAsync_MintsStamp_MultiThreaded(HashcashStamp stamp)
+    {
+        var mintedStamp = await stamp.MintAsync(_hashAlgorithm, Environment.ProcessorCount);
+
+        var isValid = mintedStamp.Verify(_hashAlgorithm);
+        isValid.Should().BeTrue();
+    }
+
     [Theory]
     [HashcashData]
     public void ToString_ReturnsStampString_GivenDetails(HashcashStamp stamp)
